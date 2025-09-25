ee571aa5f84526b978cf01f8a7bbf17734576221
BRUH-3001 <azrelsakhireswara2007@gmail.com>
2025-09-25 19:11:04 +0700

    Testing

--- Tugas Tutor.cpp
+++ Tugas Tutor.cpp
@@ -9,6 +9,14 @@ Nama Anggota Kelompok Yang Mengerjakan:
 
 using namespace std;
 
+void PenamaanMenu(int a, int b, int c){
+/* Ini buat pemilihan menu, masukkin aja cin atau cout */
+}
+
+int PemilihanMenu(int a, int b, int c){
+/* Ini untuk membuat proses penghitungan variabel menu, buat sebisa kalian aja*/
+}
+
 int main(){
 
 }
